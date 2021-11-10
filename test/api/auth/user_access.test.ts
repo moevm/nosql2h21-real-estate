cbeb4458e154af5abefa4d69233332dd4f5b6b00
milesartemius <shveitsar215@gmail.com>
2021-11-11 01:01:58 +0300

    sample auto-tests added

---
+++ test/api/auth/user_access.test.ts
@@ -0,0 +1,26 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import chai from "chai";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import chaiHttp from "chai-http";
+import { defaultUser } from "../../test_env";
+
+describe("User creation test", () => {
+  chai.use(chaiHttp);
+  const agent = chai.request.agent("http://localhost:3000");
+
+  it("Should log in user", () => {
+    return agent
+      .post("/api/auth/signin")
+      .send(defaultUser)
+      .then((res) => {
+        chai.expect(res).to.have.cookie("accessToken");
+        chai.expect(res).to.not.have.cookie("accessToken", "");
+      });
+  });
+
+  it("Should log out user", () => {
+    return agent.get("/api/auth/signout").then((res) => {
+      chai.expect(res).to.have.cookie("accessToken", "");
+    });
+  });
+});
---
+++ test/api/auth/user_lifecycle.test.ts
@@ -0,0 +1,29 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import chai from "chai";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import chaiHttp from "chai-http";
+
+describe("User creation test", () => {
+  chai.use(chaiHttp);
+  const agent = chai.request.agent("http://localhost:3000");
+
+  it("Should create user and log in", () => {
+    return agent
+      .post("/api/auth/signup")
+      .send({
+        email: "new.test.user@site.com",
+        password: "1234567",
+        firstName: "New User",
+        lastName: "Test",
+      })
+      .then((res) => {
+        // chai.expect(res).to.have.cookie("accessToken");
+      });
+  });
+
+  it("Should delete user", () => {
+    return agent.get("/api/auth/signout").then((res) => {
+      // chai.expect(res).to.not.have.cookie("accessToken");
+    });
+  });
+});
