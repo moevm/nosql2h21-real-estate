e8341c2136832a34620c4622da1412f0866d865b
Dmitriy129 <dmitriy77677@gmail.com>
2021-12-26 03:03:22 +0300

    feat(map): add ymap

--- components/organisms/AdvsMap/index.tsx
+++ components/organisms/AdvsMap/index.tsx
@@ -1,60 +1,50 @@
-import React, { memo, useEffect, useMemo, useState } from "react";
-import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
-import { observer } from "mobx-react";
-import advsStore from "stores/advs";
-import RequestStatus from "core/types/requestStatus";
 import { AdvCard } from "components/moleculs";
 import { Advertisement } from "core/models";
+import { observer } from "mobx-react";
 import { useRouter } from "next/dist/client/router";
+import React, { useEffect, useMemo, useState } from "react";
+import { YMaps, Map, Placemark } from "react-yandex-maps";
+import advsStore from "stores/advs";
 
-const containerStyle = {
-  width: "100%",
-  minHeight: "450px",
-};
-
-const defaultCenter = {
-  lat: 59.9716662,
-  lng: 30.3237567,
-};
-
-const AdvsMap: React.FC = () => {
+const defaultCenter = [59.9716662, 30.3237567];
+const AdvsMap = (): JSX.Element => {
   const [selected, setSelected] = useState<Advertisement | null>(null);
 
   const router = useRouter();
 
   const center = useMemo(
-    () => ({
-      lat: parseFloat(`${router.query.lat}`) || defaultCenter.lat,
-      lng: parseFloat(`${router.query.lng}`) || defaultCenter.lng,
-    }),
+    () => [+`${router.query.lat}` || defaultCenter[0], +`${router.query.lng}` || defaultCenter[1]],
     [router],
   );
 
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: "AIzaSyCfrpLTS2S563wrzq2LhHL6aN0DIUoBc5A",
-  });
   useEffect(() => {
     advsStore.loadAll();
   }, []);
 
-  return isLoaded ? (
+  return (
     <>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
-        <>
+      <YMaps>
+        <Map
+          defaultState={{
+            center,
+            zoom: 15,
+            controls: ["zoomControl", "fullscreenControl"],
+          }}
+          modules={["control.ZoomControl", "control.FullscreenControl"]}
+          width="100%"
+          height={400}
+        >
           {advsStore.list.map((adv) => (
-            <Marker
+            <Placemark
               key={adv._id}
-              position={{ lat: adv.house.address.lat, lng: adv.house.address.lng }}
+              defaultGeometry={[adv.house.address.lat, adv.house.address.lng]}
               onClick={() => setSelected(adv)}
             />
           ))}
-        </>
-      </GoogleMap>
+        </Map>
+      </YMaps>
       {selected && <AdvCard key={selected._id} data={selected} />}
     </>
-  ) : (
-    <></>
   );
 };
 
